3441e0cdf913ce82442909a9d546926515641b76
Ana Ruth Rivera <anaruth.riverasalgado@gmail.com>
2025-09-12 19:55:21 -0600

    Arreglos de codigo, y tipo de archivo realizado correctamente

--- DNNLibTrial.py
+++ DNNLibTrial.py
@@ -1,87 +1,30 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 17,
-   "id": "947aeef6-1ca9-4947-b020-7bf1bb7c6383",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "3 entradas, 5 salidas, con activación ReLU\n",
-      "Salida con activación: [[0.05  0.14  0.038 0.028]]\n",
-      "Salida lineal: [[0.05  0.14  0.038 0.028]]\n",
-      "Sigmoid: [0.5        0.88079708 0.26894142]\n",
-      "\n",
-      "3 entradas, 2 salidas, con activación TanH\n",
-      "Salida con activación: [[0.04995837 0.13909245]]\n",
-      "Salida lineal: [[0.05 0.14]]\n"
-     ]
-    }
-   ],
-   "source": [
-    "import numpy as np\n",
-    "import DnnLib\n",
-    "\n",
-    "entradas = np.array([[0.5, -0.2, 0.1]])\n",
-    "\n",
-    "#3 entradas, 5 salidas, con activación ReLU\n",
-    "layer1 = DnnLib.DenseLayer(3, 4, DnnLib.ActivationType.RELU)\n",
-    "layer1.weights = np.array([[0.1, 0.2, 0.3],[0.4, 0.5, 0.6],[0.1, 0.02, -0.3],[0.24, 0.41, 0.3]])\n",
-    "layer1.bias = np.array([0.01, -0.02, 0.022, -0.04])\n",
-    "\n",
-    "print(\"3 entradas, 5 salidas, con activación ReLU\")\n",
-    "# Con activación\n",
-    "y1 = layer1.forward(entradas)\n",
-    "print(\"Salida con activación:\", y1)\n",
-    "# lineal, sin activación\n",
-    "y_lin = layer1.forward_linear(entradas)\n",
-    "print(\"Salida lineal:\", y_lin)\n",
-    "# Activaciones directamente\n",
-    "print(\"Sigmoid:\", DnnLib.sigmoid(np.array([0.0, 2.0, -1.0])))\n",
-    "\n",
-    "#3 entradas, 2 salidas, con activación TanH\n",
-    "layer = DnnLib.DenseLayer(3, 2, DnnLib.ActivationType.TANH)\n",
-    "layer.weights = np.array([[0.1, 0.2, 0.3],[0.4, 0.5, 0.6]])\n",
-    "layer.bias = np.array([0.01, -0.02])\n",
-    "\n",
-    "print(\"\\n3 entradas, 2 salidas, con activación TanH\")\n",
-    "y = layer.forward(entradas)\n",
-    "print(\"Salida con activación:\", y)\n",
-    "y_lin = layer.forward_linear(entradas)\n",
-    "print(\"Salida lineal:\", y_lin)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "950b995b-7a6c-4e77-832d-8cdac7ff89c2",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.10.18"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import numpy as np
+import DnnLib
+
+entradas = np.array([[0.5, -0.2, 0.1]])
+
+#3 entradas, 5 salidas, con activación ReLU
+layer1 = DnnLib.DenseLayer(3, 4, DnnLib.ActivationType.RELU)
+layer1.weights = np.array([[0.1, 0.2, 0.3],[0.4, 0.5, 0.6],[0.1, 0.02, -0.3],[0.24, 0.41, 0.3]])
+layer1.bias = np.array([0.01, -0.02, 0.022, -0.04])
+
+print("3 entradas, 5 salidas, con activación ReLU")
+# Con activación
+y1 = layer1.forward(entradas)
+print("Salida con activación:", y1)
+# lineal, sin activación
+y_lin = layer1.forward_linear(entradas)
+print("Salida lineal:", y_lin)
+# Activaciones directamente
+print("Sigmoid:", DnnLib.sigmoid(np.array([0.0, 2.0, -1.0])))
+
+#3 entradas, 2 salidas, con activación TanH
+layer = DnnLib.DenseLayer(3, 2, DnnLib.ActivationType.TANH)
+layer.weights = np.array([[0.1, 0.2, 0.3],[0.4, 0.5, 0.6]])
+layer.bias = np.array([0.01, -0.02])
+
+print("\n3 entradas, 2 salidas, con activación TanH")
+y = layer.forward(entradas)
+print("Salida con activación:", y)
+y_lin = layer.forward_linear(entradas)
+print("Salida lineal:", y_lin)
